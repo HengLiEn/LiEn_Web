85a63c404bb034b239ccc6a592a8374357ffb2c7
Li En <henglien@gmail.com>
2026-04-12 20:56:52 +0800

    edit changes

--- api/index.py
+++ api/index.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="../templates", static_folder="../static")
+app = Flask(__name__)
+
+PHOTOS = {
+    "hero": "photo_2026-04-12-19-09-57.jpg",
+    "about": "photo_2026-04-12-19-51-34.jpg"
+}
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", photos=PHOTOS)
 
 
 # Vercel entrypoint
